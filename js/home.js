701852c47e0d9ec071ada4b807f99193eaa0d59a
Đăng Khoa <hdkhoa.work@gmail.com>
2025-07-12 16:18:53 +0700

    update

--- js/home.js
+++ js/home.js
@@ -19,3 +19,38 @@ fetch('/components/footer/index.html')
         .then(html => {
           document.getElementById('footer-placeholder').innerHTML = html
         })
+
+        // slide project
+const images = [
+    "https://swiperjs.com/demos/images/nature-1.jpg",
+    "https://swiperjs.com/demos/images/nature-2.jpg",
+    "https://swiperjs.com/demos/images/nature-3.jpg",
+    "https://swiperjs.com/demos/images/nature-4.jpg",
+    "https://swiperjs.com/demos/images/nature-5.jpg"
+  ];
+
+  const bg = document.getElementById('slideBackground');
+
+  const setBackground = (index) => {
+    bg.style.backgroundImage = `url(${images[index]})`;
+  };
+
+  const swiper = new Swiper(".swiper-projects", {
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 5,
+    loop: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    on: {
+      init(swiper) {
+        setBackground(swiper.realIndex);
+      },
+      slideChange(swiper) {
+        setBackground(swiper.realIndex);
+      }
+    }
+  });
