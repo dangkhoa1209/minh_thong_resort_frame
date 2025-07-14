41e7d3cf2740153352530f082664420ad91d3b4b
Đăng Khoa <hdkhoa.work@gmail.com>
2025-07-14 09:12:10 +0700

    ok

--- js/pages/experience.js
+++ js/pages/experience.js
@@ -1,20 +1,22 @@
- const wrapper = document.querySelector('.experience-wrapper');
-  const items = document.querySelectorAll('.experience-item');
 
-  items.forEach(item => {
-    item.addEventListener('click', () => {
-      if (item.classList.contains('active')) return;
-      items.forEach(i => i.classList.remove('active'));
-      item.classList.add('active');
-      wrapper.classList.add('open');
-    });
-  });
 
-  const closeButtons = document.querySelectorAll('.close-btn');
-  closeButtons.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-      e.stopPropagation();
-      items.forEach(i => i.classList.remove('active'));
-      wrapper.classList.remove('open');
-    });
+const wrapper = document.querySelector('.experience-wrapper');
+const items = document.querySelectorAll('.experience-item');
+
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    items.forEach(i => i.classList.remove('active'));
+    item.classList.add('active');
+    wrapper.classList.add('open');
   });
+});
+
+
+const closeButtons = document.querySelectorAll('.close-btn');
+closeButtons.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.stopPropagation();
+    items.forEach(i => i.classList.remove('active'));
+    wrapper.classList.remove('open');
+  });
+});
