147ab705b34c2b1d1570e29d2124ac4f18715fcf
Đăng Khoa <hdkhoa.work@gmail.com>
2025-07-15 11:03:19 +0700

    update

--- js/pages/experience.js
+++ js/pages/experience.js
@@ -2,12 +2,16 @@ loadComponent('/components/header/index.html', 'place-header')
 
 const wrapper = document.querySelector('.experience-wrapper');
 const items = document.querySelectorAll('.experience-item');
+const placeHeader = document.getElementById('place-header');
+
 
 items.forEach(item => {
   item.addEventListener('click', () => {
     items.forEach(i => i.classList.remove('active'));
     item.classList.add('active');
     wrapper.classList.add('open');
+    // Ẩn header
+    placeHeader.style.display = 'none';
   });
 });
 
@@ -18,5 +22,7 @@ closeButtons.forEach(btn => {
     e.stopPropagation();
     items.forEach(i => i.classList.remove('active'));
     wrapper.classList.remove('open');
+    // Hiện header lại
+    placeHeader.style.display = '';
   });
 });
