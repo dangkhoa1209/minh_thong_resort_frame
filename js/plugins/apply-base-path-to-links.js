66a448fb3524bda2f2fafb0e3e188d41bf48f131
Đăng Khoa <hdkhoa.work@gmail.com>
2025-07-16 11:56:39 +0700

    fixx

--- js/plugins/apply-base-path-to-links.js
+++ js/plugins/apply-base-path-to-links.js
@@ -3,7 +3,6 @@ const basePath = getBasePath()
 document.querySelectorAll('a[href^="/"]').forEach(a => {
   const rawHref = a.getAttribute('href');
 
-  console.log('rawHref', rawHref);
 
   a.href = basePath + rawHref;
 });
