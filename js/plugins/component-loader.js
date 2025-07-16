a43937ab0f01fe2bdaed7d71cdfde19c21b66b4b
Đăng Khoa <hdkhoa.work@gmail.com>
2025-07-16 10:12:26 +0700

    update

--- js/plugins/component-loader.js
+++ js/plugins/component-loader.js
@@ -1,4 +1,8 @@
 function loadComponent(url, containerId) {
+  const basePath = window.location.pathname.includes('/minh_thong_resort_frame/')
+    ? '/minh_thong_resort_frame'
+    : '';
+
   fetch(url)
     .then(res => res.text())
     .then(html => {
@@ -8,33 +12,48 @@ function loadComponent(url, containerId) {
 
       const cssLinks = temp.querySelectorAll('link[rel="stylesheet"]');
       const scriptTags = temp.querySelectorAll('script[src]');
+      const imgTags = temp.querySelectorAll('img[src]');
 
-      // Load CSS links
+      // Load CSS
       const cssPromises = [...cssLinks].map(link => {
-        const exists = [...document.head.querySelectorAll('link')].some(l => l.href === link.href);
+        const rawHref = link.getAttribute('href');
+        const fullHref = rawHref.startsWith('http') ? rawHref : basePath + rawHref.replace(/^(\.\/|\.\.\/)+/, '/');
+        const exists = [...document.head.querySelectorAll('link')].some(l => l.href === location.origin + fullHref);
         if (!exists) {
           return new Promise(resolve => {
-            const newLink = link.cloneNode();
+            const newLink = document.createElement('link');
+            newLink.rel = 'stylesheet';
+            newLink.href = fullHref;
             newLink.onload = resolve;
             document.head.appendChild(newLink);
           });
         } else {
-          return Promise.resolve(); // Đã có rồi
+          return Promise.resolve();
         }
       });
 
-      // Load JS scripts
+      // Load JS
       scriptTags.forEach(script => {
-        const exists = [...document.querySelectorAll('script')].some(s => s.src === script.src);
+        const rawSrc = script.getAttribute('src');
+        const fullSrc = rawSrc.startsWith('http') ? rawSrc : basePath + rawSrc.replace(/^(\.\/|\.\.\/)+/, '/');
+        const exists = [...document.querySelectorAll('script')].some(s => s.src === location.origin + fullSrc);
         if (!exists) {
           const newScript = document.createElement('script');
-          newScript.src = script.src;
+          newScript.src = fullSrc;
           newScript.defer = true;
           document.body.appendChild(newScript);
         }
       });
 
-      // Chờ CSS load xong rồi mới append nội dung
+      // Fix image srcs if needed
+      imgTags.forEach(img => {
+        const rawSrc = img.getAttribute('src');
+        if (!rawSrc.startsWith('http')) {
+          img.src = basePath + rawSrc.replace(/^(\.\/|\.\.\/)+/, '/');
+        }
+      });
+
+      // Load content after CSS
       Promise.all(cssPromises).then(() => {
         const content = temp.querySelector('section') || temp.firstElementChild;
         if (content) container.appendChild(content);
