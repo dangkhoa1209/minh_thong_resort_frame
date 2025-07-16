40d1a3c402a2544f5e7310beb825f21f77590e3b
Đăng Khoa <hdkhoa.work@gmail.com>
2025-07-16 11:14:46 +0700

    update

--- js/plugins/component-loader.js
+++ js/plugins/component-loader.js
@@ -1,12 +1,10 @@
 function loadComponent(url, containerId) {
-  const basePath = getBasePath()
-
-  console.log('basePath', basePath);
-  
-
   fetch(url)
     .then(res => res.text())
     .then(html => {
+      const basePath = getBasePath()
+      console.log('basePath', basePath);
+
       const container = document.getElementById(containerId);
       const temp = document.createElement('div');
       temp.innerHTML = html;
