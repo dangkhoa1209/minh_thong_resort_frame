4dc61591aed3945a819cc8e04f1857a2b38a0fdb
Đăng Khoa <hdkhoa.work@gmail.com>
2025-07-16 10:51:02 +0700

    fix

--- js/plugins/base-path.js
+++ js/plugins/base-path.js
@@ -1,9 +1,6 @@
-function getBasePath(p) { 
-  
-  const path = p | 'minh_thong_resort_frame'
-  const basePath = window.location.pathname.includes(`/${path}/`)
-  ? `/${path}`
-  : '';
-
-  return basePath
+function getBasePath(p = 'minh_thong_resort_frame') {
+  const basePath = window.location.pathname.includes(`/${p}/`)
+    ? `/${p}`
+    : '';
+  return basePath;
 }
