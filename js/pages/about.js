385f6d7dd44e919dcec39bdeb53ba66b20f94c3d
Đăng Khoa <hdkhoa.work@gmail.com>
2025-07-22 15:22:10 +0700

    update

---
+++ js/pages/about.js
@@ -0,0 +1,22 @@
+loadComponent('../components/header/index.html', 'place-header')
+loadComponent('../components/footer/index.html', 'place-footer')
+
+document.addEventListener("DOMContentLoaded", () => {
+  const blocks = document.querySelectorAll(".production__block");
+
+  function adjustHeights() {
+    blocks.forEach(block => {
+      const image = block.querySelector(".production__image");
+      const text = block.querySelector(".production__text");
+
+      if (image && text) {
+        const imageHeight = image.offsetHeight;
+        text.style.minHeight = imageHeight + "px";
+      }
+    });
+  }
+
+  // Gọi khi load và khi resize
+  adjustHeights();
+  window.addEventListener("resize", adjustHeights);
+});
