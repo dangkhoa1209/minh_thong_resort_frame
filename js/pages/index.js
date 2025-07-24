48ad7dad82d1076988bc8a16d801e6eb2dc599f4
Đăng Khoa <hdkhoa.work@gmail.com>
2025-07-24 11:56:19 +0700

    Revert "update"
    This reverts commit e252975a2c683d7ebdd918e64c3c4ecf09eda9c3.

--- js/pages/index.js
+++ js/pages/index.js
@@ -47,11 +47,10 @@ logo.addEventListener('animationend', () => {
 });
 
 window.addEventListener('scroll', () => {
-  // if (!logo.classList.contains('animation-done')) return;
+  if (!logo.classList.contains('animation-done')) return; // chưa xong animation thì bỏ qua
   const scrollY = window.scrollY;
   bg.style.transform = `translateY(${scrollY * 0.3}px)`;
   nonbg.style.transform = `translateY(${scrollY * -0.1}px)`;
-  
-  logo.style.transform = `translate(-50%, calc(-50% + ${Math.min(scrollY, 500) * 0.8}px)) scale(1)`;
+  logo.style.transform = `translate(-50%, calc(-50% + ${scrollY * 0.8}px)) scale(1)`;
   
 });
