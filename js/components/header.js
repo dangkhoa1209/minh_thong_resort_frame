91046a124eb3576c18bbf408fa5071a5037d3238
Đăng Khoa <hdkhoa.work@gmail.com>
2025-07-24 14:37:32 +0700

    update

--- js/components/header.js
+++ js/components/header.js
@@ -8,7 +8,7 @@ const logos = document.querySelectorAll('.logo')
 
 logos?.forEach(logo => {
   logo.addEventListener('click', () => {
-    const basePath = getBasePath()    
+    const basePath = getBasePath()
     window.location.href = basePath || '/';
 
     // if (barbaGoHeader) {
@@ -20,22 +20,48 @@ logos?.forEach(logo => {
 });
 
 
-  const menuTrigger = document.querySelector("#header-menu");
-  const drawer = document.querySelector(".header-drawer");
-  const closeBtn = document.querySelector(".header-drawer__close");
-  const overlay = document.querySelector(".header-drawer__overlay");
+const menuTrigger = document.querySelector("#header-menu");
+const drawer = document.querySelector(".header-drawer");
+const closeBtn = document.querySelector(".header-drawer__close");
+const overlay = document.querySelector(".header-drawer__overlay");
 
-  const openDrawer = () => drawer.classList.add("active");
-  const closeDrawer = () => drawer.classList.remove("active");
+const openDrawer = () => {
+  drawer.classList.add("active");
+  document.body.style.overflow = "hidden"; // Chặn scroll
+};
 
-  if (menuTrigger && drawer) {
-    menuTrigger.addEventListener("click", openDrawer);
+const closeDrawer = () => {
+  drawer.classList.remove("active");
+  document.body.style.overflow = ""; // Khôi phục scroll
+};
+
+if (menuTrigger && drawer) {
+  menuTrigger.addEventListener("click", openDrawer);
+}
+
+if (closeBtn) closeBtn.addEventListener("click", closeDrawer);
+if (overlay) overlay.addEventListener("click", closeDrawer);
+
+// Đóng khi nhấn ESC
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") closeDrawer();
+});
+
+
+//Scroll
+let lastScrollY = window.scrollY;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY > lastScrollY && currentScrollY > 100) {
+    // Cuộn xuống -> ẩn header
+    header.classList.add('hide');
+  } else {
+    // Cuộn lên -> hiện header
+    header.classList.remove('hide');
   }
 
-  if (closeBtn) closeBtn.addEventListener("click", closeDrawer);
-  if (overlay) overlay.addEventListener("click", closeDrawer);
-
-  // Đóng khi nhấn ESC
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") closeDrawer();
-  });
+  lastScrollY = currentScrollY;
+});
