4a2b81ccb7e8d64516394220224e29aafb38ed4c
Đăng Khoa <hdkhoa.work@gmail.com>
2025-07-29 15:10:16 +0700

    update

--- js/components/footer.js
+++ js/components/footer.js
@@ -1,40 +1,54 @@
-const arrow = document.querySelector(".footer-arrow");
-const input = document.querySelector(".footer-input");
-const status = document.getElementById("footer-form-status");
-arrow.addEventListener("click", async () => {
-  const email = input.value.trim();
-
-  status.textContent = "Sending...";
-  status.style.color = "#888";
-  status.classList.add('active');
-
-  // Validate email
-  if (!email || !/^\S+@\S+\.\S+$/.test(email)) {
-    status.textContent = "Please enter a valid email.";
-    status.style.color = "red";
-    return;
-  }
+let arrow = document.querySelector(".footer-arrow");
+let input = document.querySelector(".footer-input");
+let statusFeild = document.getElementById("footer-form-status");
 
 
-
-  try {
-    // Gửi request tới server (thay URL bằng API thật)
-    const response = await fetch("https://your-backend.com/newsletter", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email }),
-    });
-
-    if (response.ok) {
-      status.textContent = "Thank you! Your email has been sent.";
-      status.style.color = "green";
-      input.value = "";
-    } else {
-      throw new Error("Server error");
+if (!arrow || !input || !statusFeild) {
+  setTimeout(() => {
+     arrow = document.querySelector(".footer-arrow");
+     input = document.querySelector(".footer-input");
+     statusFeild = document.getElementById("footer-form-status");
+    if (!arrow || !input || !statusFeild) {
+      return
     }
-  } catch (error) {
-    status.textContent = "Failed to send. Please try again.";
-    status.style.color = "red";
-  }
-});
 
+    start()
+  }, 500)
+}
+function start() {
+  arrow.addEventListener("click", async () => {
+    const email = input.value.trim();
+
+    statusFeild.textContent = "Sending...";
+    statusFeild.style.color = "#888";
+    statusFeild.classList.add('active');
+
+    // Validate email
+    if (!email || !/^\S+@\S+\.\S+$/.test(email)) {
+      statusFeild.textContent = "Please enter a valid email.";
+      statusFeild.style.color = "red";
+      return;
+    }
+
+
+
+    try {
+      // Gửi request tới server (thay URL bằng API thật)
+      const response = await fetch("https://your-backend.com/newsletter", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        statusFeild.textContent = "Thank you! Your email has been sent.";
+        statusFeild.style.color = "green";
+        input.value = "";
+      } else {
+      }
+    } catch (error) {
+      statusFeild.textContent = "Failed to send. Please try again.";
+      statusFeild.style.color = "red";
+    }
+  });
+}
